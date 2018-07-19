4d3de45c09e7bbd5a50cd5d017e801bc6e9ab29a
leon-fe <leon@greatfeatservices.com>
2018-07-19 10:56:27 +0800

    demo

--- middleware/auth.js
+++
@@ -1,5 +0,0 @@
-export default function ({userAgent, req}) {
-  // console.log('process.server', process.server)
-  if (process.server && !req) return
-  userAgent = process.server ? req.headers['user-agent'] : navigator.userAgent
-}
--- plugins/element-ui.js
+++
@@ -1,5 +0,0 @@
-import Vue from 'vue'
-import { Message } from 'element-ui'
-import 'element-ui/lib/theme-chalk/message.css'
-
-Vue.prototype.$message = Message
